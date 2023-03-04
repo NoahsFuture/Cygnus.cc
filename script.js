1e2806850b46ad09c616fc639a39343378cf333e
haonski <44350435+NoahsFuture@users.noreply.github.com>
2023-03-04 01:22:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ particlesJS('particles-js', {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#000000"
+        "color": "##760076"
       },
       "polygon": {
         "nb_sides": 5
